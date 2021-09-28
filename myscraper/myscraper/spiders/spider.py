82d2a6068446cc6e3755773610e3ebccafb5ea03
bigbanggk01 <phamthachtu196@gmail.com>
2021-09-28 14:51:54 +0700

    dynamic web scraping feature

---
+++ myscraper/myscraper/spiders/spider.py
@@ -0,0 +1,81 @@
+import scrapy
+from myscraper.items import MyscraperItem
+from scrapy_splash import SplashRequest
+
+class productSpider(scrapy.Spider):
+    name = "products"
+    script_1 = """
+    function main(splash)
+        splash:init_cookies(splash.args.cookies)
+
+        local num_scrolls = 10
+        local scroll_delay = 1.0
+
+        local scroll_to = splash:jsfunc("window.scrollTo")
+        local get_body_height = splash:jsfunc(
+            "function() {return document.body.scrollHeight;}"
+        )
+        assert(splash:go(splash.args.url))
+        splash:wait(splash.args.wait)
+
+        for _ = 1, num_scrolls do
+            local height = get_body_height()
+            for i = 1, 10 do
+                scroll_to(0, height * i/10)
+                splash:wait(scroll_delay/10)
+            end
+        end        
+        return {
+            cookies = splash:get_cookies(),
+            html = splash:html()
+        }
+    end
+    """
+    script_2 = """
+    function main(splash)
+        splash:init_cookies(splash.args.cookies)
+        local url = splash.args.url
+        assert(splash:go(url))
+        splash:wait(splash.args.wait)
+
+        return {
+            cookies = splash:get_cookies(),
+            html = splash:html()
+        }
+    end
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.urls = kwargs['urls']
+        self.items_xpath = kwargs['items_xpath']
+        self.url_xpath = kwargs['url_xpath']
+        self.name_xpath = kwargs['name_xpath']
+        self.price_xpath = kwargs['price_xpath']
+        self.image_xpath = kwargs['image_xpath']
+    def start_requests(self):
+        meta = {
+            'items_xpath': self.items_xpath,
+            'url_xpath' : self.url_xpath, 
+            'name_xpath': self.name_xpath, 
+            'price_xpath': self.price_xpath, 
+            'image_xpath': self.image_xpath,
+            }
+        for url in self.urls:
+            if url[1] == 1:
+                yield scrapy.Request(url[0], callback=self.parse, meta=meta)
+                
+            elif url[1] == 2:
+                yield SplashRequest(url[0], self.parse, endpoint='execute',
+                                args={'wait':2,'lua_source': self.script_1},meta=meta)
+            elif url[1] == 3:
+                yield SplashRequest(url[0], self.parse, endpoint='execute',
+                                args={'wait':2,'lua_source': self.script_2},meta=meta)
+    def parse(self, response):
+        item = MyscraperItem()
+        products = response.xpath(response.meta['items_xpath'])
+        for product in products:
+            item['url'] = product.xpath(response.meta['url_xpath']).get()
+            item['name'] = product.xpath(response.meta['name_xpath']).get()
+            item['price'] = product.xpath(response.meta['price_xpath']).get()
+            item['image_link'] = product.xpath(response.meta['image_xpath']).get()
+            yield item
---
+++ myscraper/startspider.py
@@ -0,0 +1,51 @@
+from myscraper.spiders import spider
+from scrapy.crawler import CrawlerProcess
+class runner():
+    def __init__(self,urls,items_xpath,url_xpath,name_xpath,price_xpath,image_xpath):
+        self.settings = {
+            'SPASH_URL': 'http://localhost:8050',
+            'DOWNLOADER_MIDDLEWARES' : {
+                'scrapy_splash.SplashCookiesMiddleware': 723,
+                'scrapy_splash.SplashMiddleware': 725,
+                'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+            },
+            'SPIDER_MIDDLEWARES' : {
+                'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+            },
+            'DUPEFILTER_CLASS' : 'scrapy_splash.SplashAwareDupeFilter',
+            'HTTPCACHE_STORAGE' : 'scrapy_splash.SplashAwareFSCacheStorage',
+            'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+            'ROBOTSTXT_OBEY' : 'False',
+            'BOT_NAME' : 'ProductScrapyBot',
+            'DEFAULT_REQUEST_HEADERS' : {
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'en',
+            },
+            'DOWNLOAD_TIMEOUT' : '180',
+            'LOG_ENABLED' : 'True',
+        }
+        self.process = CrawlerProcess(self.settings)
+        self.process.crawl(
+            spider.productSpider,
+            urls = urls,
+            items_xpath = items_xpath,
+            url_xpath = url_xpath,
+            name_xpath = name_xpath,
+            price_xpath = price_xpath,
+            image_xpath=image_xpath
+        )
+
+    def run(self):
+        self.process.start()
+        
+r = runner(
+    [
+        ['https://www.lazada.vn/catalog/?q=dien+thoai+nokia', 3]
+    ],
+    '//div[@data-qa-locator="product-item"]',
+    './/a[1]/@href',
+    './/a[1]/@title',
+    './/span[@class="Q78Jz"]/text()',
+    './/a[1]/img/src[1]'
+)
+r.run()
